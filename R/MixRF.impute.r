df5c7817a7721538c37c1725b4961b5d7ab4fd73
Jiebiao Wang <randel.wang@gmail.com>
2017-05-06 07:56:20 -0500

    changed eqtl.lis to eqtl.lis.i when calling MixRF.impute.single()

--- R/MixRF.impute.r
+++ R/MixRF.impute.r
@@ -37,111 +37,111 @@
 #'
 MixRF.impute <- function(Ydat, eqtl.lis, snp.dat, cov=NULL, iPC=TRUE, idx.selected.gene.iPC=NULL,
                          parallel.size=1, correlation=FALSE, nCV=3){
-
+  
   MixRF.impute.single <- function(Yi, eqtl.lis.i, snp.dat, cov=NULL, iPC.cov=NULL){
-
+    
     MixRF <- function(data, initialRandomEffects=0,
                       ErrorTolerance=0.001, MaxIterations=1000) {
-
+      
       Target = data$Y
       ID = data$ID
-
+      
       # Condition that indicates the loop has not converged or run out of iterations
       ContinueCondition <- TRUE
-
+      
       iterations <- 0
-
+      
       # Get initial values
       AdjustedTarget <- Target - initialRandomEffects
       oldvar <- -Inf
-
+      
       # y - X*beta
       resi = rep(NA,length(Target))
-
+      
       if(ncol(data)==3) {
         x = as.data.frame(data[,-c(1:2)])
         colnames(x) = colnames(data)[3]
       } else x = data[,-c(1:2)]
-
-
+      
+      
       while(ContinueCondition){
-
+        
         iterations <- iterations+1
-
+        
         # randomForest
         rf = randomForest(x,AdjustedTarget)
-
+        
         # out-of-bag prediction
         resi = Target - rf$predicted
-
+        
         ## Estimate New Random Effects and Errors using lmer
         lmefit <- lmer(resi~1+(1|ID), data=data)
-
+        
         # check on convergence
         newvar <- attr(VarCorr(lmefit)$'ID','stddev')^2
-
+        
         ContinueCondition <- (newvar-oldvar>ErrorTolerance & iterations < MaxIterations)
         oldvar <- newvar
-
+        
         # Extract random effects to make the new adjusted target
         AllEffects <- predict(lmefit)-fixef(lmefit)
         #  y-Zb
         AdjustedTarget <- Target - AllEffects
       }
-
+      
       result <- list(forest=rf, EffectModel=lmefit, RandomEffects=ranef(lmefit),
                      IterationsUsed=iterations, ErrorTolerance=ErrorTolerance)
-
+      
       return(result)
     }
-
-
-
+    
+    
+    
     predict_MixRF <- function(object, newdata, id=NULL, EstimateRE=TRUE){
-
+      
       # Base predictions from the forest part
       if(ncol(newdata)==3) {
         x = as.data.frame(newdata[,-c(1:2)])
         colnames(x) = colnames(newdata)[3]
       } else x = newdata[,-c(1:2)]
-
+      
       forestPrediction <- predict(object$forest,x)
-
+      
       # If we aren't estimating random effects, we
       # just use the forest for prediction.
       if(!EstimateRE){
         return(forestPrediction)
       }
-
+      
       # Get the group identifiers if necessary
       if(is.null(id)){
         id <- as.matrix(object$EffectModel$groups)
       }
-
+      
       n = length(unique(id))
       nT = length(forestPrediction)/n
-
+      
       completePrediction <- matrix(forestPrediction, n, nT)
-
+      
       # Get the identities of the groups in the data
       uniqueID <- unique(id)
-
+      
       estRE <- matrix(0, nrow=n, ncol=1)
       oRE=object$RandomEffects[[1]]
       idx.re=as.integer(rownames(oRE))
       estRE[idx.re,1] <- as.vector(oRE[[1]])
-
+      
       for(i in uniqueID){
         completePrediction[i,] = completePrediction[i,] + estRE[i,1]
       }
-
+      
       return(completePrediction)
     }
-
-
+    
+    
     n=nrow(Yi)
     nT=ncol(Yi)
-
+    
     # transform covariates array into a matrix
     covMat=NULL
     if (!is.null(iPC.cov)){
@@ -152,95 +152,95 @@ MixRF.impute <- function(Ydat, eqtl.lis, snp.dat, cov=NULL, iPC=TRUE, idx.select
         covMat = cbind(covMat,oo)
       }
     }
-
+    
     y_mean = apply(Yi,2,mean,na.rm=T)
-
+    
     Ymat = matrix(Yi-matrix(rep(y_mean,n),byrow=T,nrow=n),ncol=1)
     ID = rep(1:n, nT)
-
+    
     # Marginal effects: regress each tissue and gene on each eQTL one by one
     ID2 = rep(1:nT, rep(n,nT))
-
+    
     comCovMat <- NULL
     if (!is.null(cov)) {
       for (i in 1:nT) comCovMat=rbind(comCovMat, cov)
     }
-
+    
     XTmat = comCovMat
-
+    
     ## include all eQTLs, then adaptively weight them
-
-
+    
+    
     if(!is.null(eqtl.lis.i)) {
       eqtl.lis.i <- unlist(eqtl.lis.i)
       XTmat= cbind(XTmat, matrix(snp.dat[,eqtl.lis.i], ncol=length(eqtl.lis.i))[ID,])
     }
-
+    
     ## adaptive.weight
     XTmat_marginal = XTmat
-
+    
     for (i in 1:nT){
       for(j in 1:ncol(XTmat)){
         if(sum(!is.na(Ymat[ID2==i]))>0){
           try({lm_marginal = lm(Ymat[ID2==i] ~ XTmat[ID2==i,j],na.action=na.exclude)},silent=T)
           weight = coef(lm_marginal)[2]
-
+          
           # coef may be NA since all X are 0s
           if(!is.na(weight)) XTmat_marginal[ID2==i,j] = XTmat[ID2==i,j] * weight
         }
       }
     }
-
+    
     XX = cbind(covMat, XTmat_marginal)
-
+    
     newdata = as.data.frame(cbind(ID, Ymat, XX))
     colnames(newdata) <- c('ID',"Y",paste0("X",1:ncol(XX)))
     xnam = colnames(newdata)[-c(1:2)]
-
+    
     dat = newdata[!is.na(Ymat),]
-
+    
     ### MixRF
     result <- MixRF(data=dat)
     fitted.y = predict_MixRF(result, newdata, id=newdata$ID, EstimateRE=TRUE)
-
+    
     #   resid.y = Ymat - as.vector(fitted.y)
-
+    
     Ypred= fitted.y + rep(y_mean,rep(n,nT))
-
+    
     return(Ypred)
   }
-
-
+  
+  
   snp.dat = snp.dat[,unique(unlist(eqtl.lis))]
   iPC.cov = NULL
   g = ncol(Ydat)
-
+  
   #set up parallel backend
   cl <- makeCluster(parallel.size)
   registerDoParallel(cl)
-
+  
   if(iPC==TRUE) {
     g = length(idx.selected.gene.iPC)
-
+    
     #loop
     ls <- foreach(i=idx.selected.gene.iPC) %dopar% {
-      MixRF.impute.single(Yi=Ydat[,i,], eqtl.lis=eqtl.lis[[i]], snp.dat=snp.dat, cov=cov,
+      MixRF.impute.single(Yi=Ydat[,i,], eqtl.lis.i=eqtl.lis[[i]], snp.dat=snp.dat, cov=cov,
                           iPC.cov=NULL)
     }
-
+    
     Yimp = Ydat[,idx.selected.gene.iPC,]
     for(i in 1:g){
       Yimp[,i,] = ls[[i]]
     }
     ### observed values
     Yimp[!is.na(Ydat[,idx.selected.gene.iPC,])] = Ydat[,idx.selected.gene.iPC,][!is.na(Ydat[,idx.selected.gene.iPC,])]
-
+    
     # get PCs
     npc = 5
     nT=dim(Yimp)[3]
     n=dim(Yimp)[1]
     iPC.cov = array(NA,dim=c(n,npc,nT))
-
+    
     for (ti in 1:nT) {
       gid=1:(dim(Yimp)[2])
       Yt=Yimp[, gid, ti]
@@ -248,34 +248,34 @@ MixRF.impute <- function(Ydat, eqtl.lis, snp.dat, cov=NULL, iPC=TRUE, idx.select
       ev.ti=matrix(NA, n, npc)
       ss= svd(t(Yt[av.sampt,]))
       ev.ti[av.sampt,] = ss$v[,1:npc]
-
+      
       iPC.cov[,,ti] = ev.ti
     }
-
+    
   }
-
+  
   # loop
   ls <- foreach(i=1:g) %dopar% {
-    MixRF.impute.single(Yi=Ydat[,i,], eqtl.lis=eqtl.lis[[i]], snp.dat, cov=cov,
+    MixRF.impute.single(Yi=Ydat[,i,], eqtl.lis.i=eqtl.lis[[i]], snp.dat, cov=cov,
                         iPC.cov=iPC.cov)
   }
-
+  
   Yimp = Ydat
   for(i in 1:g){
     Yimp[,i,] = ls[[i]]
   }
   ### observed values
   Yimp[!is.na(Ydat)] = Ydat[!is.na(Ydat)]
-
-
+  
+  
   if(correlation) {
     ### use cross-validation to estimate correlation
-
+    
     ## This function will split the data into nCV fold, and return the list of CV samples.
     ## When the option no.NA is true, we will find the split that each sample has at least one tissue observed.
-
+    
     get.CVlist = function(nCV, Ydat, no.NA=TRUE) {
-
+      
       nT = dim(Ydat)[3]
       if (no.NA==FALSE){
         indi.lis=list()
@@ -293,7 +293,7 @@ MixRF.impute <- function(Ydat, eqtl.lis, snp.dat, cov=NULL, iPC=TRUE, idx.select
           Exp.test = Ydat[,1,]
           for (ti in 1:nT) Exp.test[indi.lis[[ti]][[k]], ti] = NA
           samp.na = rowSums(!is.na(Exp.test))
-
+          
           # add a random tissue to a subject without any tissues
           if(sum(samp.na==0)>0) {
             for(i in which(samp.na==0)) {
@@ -306,11 +306,11 @@ MixRF.impute <- function(Ydat, eqtl.lis, snp.dat, cov=NULL, iPC=TRUE, idx.select
       indi.lis=lapply(indi.lis,function(x) lapply(x, function(z) sort(z)))
       return(indi.lis)
     }
-
+    
     indi.lis_k = get.CVlist(nCV, Ydat, no.NA=TRUE)
-
+    
     Yimp_cv = array(NA, dim=dim(Ydat))
-
+    
     for(k in 1:nCV) {
       print(k)
       nT = dim(Ydat)[3]
@@ -318,9 +318,9 @@ MixRF.impute <- function(Ydat, eqtl.lis, snp.dat, cov=NULL, iPC=TRUE, idx.select
       for (ti in 1:nT) {
         Ymis_k[indi.lis_k[[ti]][[k]], , ti] = NA
       }
-
+      
       ls <- foreach(i=1:g) %dopar% {
-        MixRF.impute.single(Yi=Ymis_k[,i,], eqtl.lis=eqtl.lis[[i]], snp.dat, cov=cov,
+        MixRF.impute.single(Yi=Ymis_k[,i,], eqtl.lis.i=eqtl.lis[[i]], snp.dat, cov=cov,
                             iPC.cov=iPC.cov)
       }
       Yimp_k = Ymis_k
@@ -333,7 +333,7 @@ MixRF.impute <- function(Ydat, eqtl.lis, snp.dat, cov=NULL, iPC=TRUE, idx.select
     }
     cor_g = sapply(1:g, function(i) cor(as.vector(Yimp_cv[,i,]), as.vector(Ydat[,i,]), method='spearman', use='pairwise'))
   }
-
+  
   stopCluster(cl)
   if(correlation) return(list(Yimp=Yimp, cor=cor_g)) else return(Yimp)
 }
